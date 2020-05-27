5bc575d47019d6939bfcc0a949adb0f13b1f7174
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-05-28 03:19:27 +0530

    Display list of monsters

--- src/App.js
+++ src/App.js
@@ -7,25 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      logoText: "Me Mogambo! Mogambo God!"
+      monsters: [
+        {
+          name: "Frank",
+          id: 1
+        },
+        {
+          name: "zomboe",
+          id: 2
+        },
+        {
+          name: "Mogambo",
+          id: 3
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.logoText}
-          </p>
-          <button onClick={() => {
-            this.setState({
-              logoText: "Me Mogambo! Mogambo God! Mogambo setting text"
-            })
-          }}>Change Text</button>
-
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (<h1 key={monster.id}>{monster.name}</h1>)
+        })}
       </div>
     )
   }
