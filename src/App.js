2e8620ec2c99f18e599e46abc22e7f703b6d483a
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-05-28 00:33:03 +0530

    Using state and setState

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logoText: "Me Mogambo! Mogambo God!"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Mogambo kush hua again!!
+            {this.state.logoText}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => {
+            this.setState({
+              logoText: "Me Mogambo! Mogambo God! Mogambo setting text"
+            })
+          }}>Change Text</button>
+
         </header>
       </div>
     )
