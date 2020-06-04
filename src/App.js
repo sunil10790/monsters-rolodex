0005780957448c6a86e4057fb49ddc082b1b0303
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-06-04 21:53:28 +0530

    Create Search Box Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component'
 
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -22,6 +23,10 @@ class App extends Component {
        }));
   }
 
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) => {
@@ -30,11 +35,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          type='search'
-          placeholder='Search Monsters'
-          onChange={(e) => this.setState({searchField: e.target.value})} >
-        </input>
+        <SearchBox 
+          placeholder = 'Search Monsters'
+          handleChange = {this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     )
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css'
 
 export const Card = (props) => {
     return(
-        <div class='card-container'>
+        <div className='card-container'>
             <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
             <h2>{props.monster.name}</h2>
             <p>{props.monster.email}</p>
