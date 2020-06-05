f138522906ad2af874d622a13b88df588c9f4ea2
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-06-05 18:56:52 +0530

    Add Title and Background

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1> Monsters Rolodex </h1>
         <SearchBox 
           placeholder = 'Search Monsters'
           handleChange = {this.handleChange}
